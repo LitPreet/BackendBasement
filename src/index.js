a8d34212f46166295aa14bc865d3f55179b196b1
preet006 <preetbhardwaj146@gmail.com>
2024-09-02 12:02:32 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import express from 'express'
 
+const app = express()
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`app listening on ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MOONGO db connection failed",err)
+})
 
 //first approach to connect to database
 
